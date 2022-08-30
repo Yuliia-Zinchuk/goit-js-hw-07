8fc16891aa6b0f651d0b752ee0e35f277836e966
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-08-30 04:17:16 +0200

    Add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+console.log(galleryContainer);
+const cardsGallery = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", cardsGallery);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const instance = basicLightbox.create(`
+      <img src='${event.target.dataset.source}' >
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  });
+}
